050cfa9fecc36d7a3931ce4e335ad407b1e3e86c
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2022-11-29 20:35:31 +0000

    min

--- app/Models/CollectionExample.php
+++ app/Models/CollectionExample.php
@@ -7,10 +7,15 @@ class CollectionExample
     public function example()
     {
         $data = [
-          ['price'=>10000],
-          ['price'=>25000],
-          ['price'=>30000],
+          ['price'=>10000, 'tax' => 500, 'active' => false],
+          ['price'=>20000, 'tax' => 700, 'active' => true],
+          ['price'=>30000, 'tax' => 900, 'active' => true],
         ];
-        return collect($data)->min('price');
+        return collect($data)->min(function ($value){
+            if (!$value['active']){
+                return null;
+            }
+            return $value['price'] + $value['tax'];
+        });
     }
 }
