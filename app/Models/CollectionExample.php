2b533cc622bff0011439935bf1648c11e22f2e54
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2022-11-30 23:57:40 +0000

    wrap

--- app/Models/CollectionExample.php
+++ app/Models/CollectionExample.php
@@ -2,17 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Collection;
+
 class CollectionExample
 {
     public function example()
     {
-        return collect([
-            'value1' => 'first1',
-            'value2' => 'first2',
-        ])
-            ->map(function ($value, $key){
-                return $value.$key;
-            });
+        return $this->everyThree([1,2,3,4,5],[6,7,8,9,10],[34,54,56,6,68]);
+    }
 
+    public function everyThree(...$collections)
+    {
+        return collect($collections)->flatMap(function ($item){
+           return Collection::wrap($item);
+        });
     }
 }
