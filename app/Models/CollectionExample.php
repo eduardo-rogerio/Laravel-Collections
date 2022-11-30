4ff95460f4e532f4d1ba7cd7b4eefbc3e99786b4
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2022-11-30 22:15:59 +0000

    tap

--- app/Models/CollectionExample.php
+++ app/Models/CollectionExample.php
@@ -6,10 +6,15 @@ class CollectionExample
 {
     public function example()
     {
-        $data = collect([10,25,50]);
-        return $data
-            ->diffUsing([.1,.25],function ($a,$b){
-                return ($a==(int)($b* 100)?0:-1);
-            });
+        return collect([1,2,3])
+            ->reverse()
+            ->tap(function ($collection){
+                $collection
+                    ->reverse()
+                    ->each(function ($value){
+                    dump('In Tap: '.$value);
+                });
+            })
+            ->first();
     }
 }
