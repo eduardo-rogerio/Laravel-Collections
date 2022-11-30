a6cb1abdf8a1d87d8f90ebc349112950b3284cd1
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2022-11-30 23:08:12 +0000

    map

--- app/Models/CollectionExample.php
+++ app/Models/CollectionExample.php
@@ -6,15 +6,13 @@ class CollectionExample
 {
     public function example()
     {
-        return collect([1,2,3])
-            ->reverse()
-            ->tap(function ($collection){
-                $collection
-                    ->reverse()
-                    ->each(function ($value){
-                    dump('In Tap: '.$value);
-                });
-            })
-            ->first();
+        return collect([
+            'value1' => 'first1',
+            'value2' => 'first2',
+        ])
+            ->map(function ($value, $key){
+                return $value.$key;
+            });
+
     }
 }
