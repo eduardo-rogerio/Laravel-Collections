515f45280158564a2e5749f72d6c8ca8caf2b2f9
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2022-12-01 00:59:58 +0000

    plunk

--- app/Models/CollectionExample.php
+++ app/Models/CollectionExample.php
@@ -8,9 +8,20 @@ class CollectionExample
 {
     public function example()
     {
-        return collect([1,2,3,4])->filter(function ($value,$key){
-            return $key > 2;
+//        return collect([
+//            ['product' => 'apples', 'price'=>50,'quantity'=>5],
+//            ['product' => 'bananas', 'price'=>60,'quantity'=>10],
+//            ['product' => 'oranges', 'price'=>70,'quantity'=>15],
+//            ['product' => 'coconuts', 'price'=>80,'quantity'=>25],
+//        ])->pluck('product','quantity');
+
+        return collect([
+            ['product' => 'apples', 'price'=>50,'quantity'=>5],
+            ['product' => 'bananas', 'price'=>60,'quantity'=>10],
+            ['product' => 'oranges', 'price'=>70,'quantity'=>15],
+            ['product' => 'coconuts', 'price'=>80,'quantity'=>25],
+        ])->map(function ($item){
+            return collect($item)->only(['product','quantity'])->all();
         });
     }
-
 }
