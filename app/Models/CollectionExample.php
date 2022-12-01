5f0a4aea8d388a3a43b63095c26582cfa8d3ae67
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2022-12-01 01:32:41 +0000

    each

--- app/Models/CollectionExample.php
+++ app/Models/CollectionExample.php
@@ -8,9 +8,13 @@ class CollectionExample
 {
     public function example()
     {
-        return collect(['A-43','B45','B-23','A12'])->sort(function ($a, $b){
-            $code = str_replace('-','',$a);
-            return ($code < $b) ? -1 : 1;
-        });
+        return collect([
+            ['bananas',45,'California'],
+            ['apples',20,'Florida'],
+            ['coconuts',60,'Texas'],
+        ])
+            ->each(function ($value){
+                dump("We have {$value[1]} {$value[0]} in our {$value[2]} store");
+            });
     }
 }
