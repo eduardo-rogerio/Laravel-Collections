70f2c8ae805521e7920b81b7dfa58684b4f5d92d
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2022-12-01 01:55:46 +0000

    times

--- app/Models/CollectionExample.php
+++ app/Models/CollectionExample.php
@@ -8,13 +8,10 @@ class CollectionExample
 {
     public function example()
     {
-        return collect([
-            ['bananas',45,'California'],
-            ['apples',20,'Florida'],
-            ['coconuts',60,'Texas'],
-        ])
-            ->eachSpread(function ($product,$qty,$location){
-                dump("We have {$qty} {$product} in our {$location} store");
-            });
+        return Collection::times(3,function ($value){
+            return User::factory()->make([
+                'name' => "{$value}Cool Name"
+            ]);
+        })->toArray();
     }
 }
