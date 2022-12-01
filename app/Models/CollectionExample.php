be1f088f839a6dfba696ea0f012ac609d5cefb2d
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2022-12-01 00:17:37 +0000

    filter

--- app/Models/CollectionExample.php
+++ app/Models/CollectionExample.php
@@ -8,13 +8,9 @@ class CollectionExample
 {
     public function example()
     {
-        return $this->everyThree([1,2,3,4,5],[6,7,8,9,10],[34,54,56,6,68]);
-    }
-
-    public function everyThree(...$collections)
-    {
-        return collect($collections)->flatMap(function ($item){
-           return Collection::wrap($item);
+        return collect([1,2,3,4])->filter(function ($value,$key){
+            return $key > 2;
         });
     }
+
 }
